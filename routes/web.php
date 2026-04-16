3aeeacfdb9e527c60060795aba852f116cf3be0a
Diogo Souza <diogohg24@gmail.com>
2026-04-16 20:38:35 -0300

    adcionado a rotas e o controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
