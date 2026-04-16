6bae7e2ced3d00b04fcf7b62304e7eec2a297b16
Diogo Souza <diogohg24@gmail.com>
2026-04-16 20:52:07 -0300

    Arrumando rota

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\FlavorController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('flavors', FlavorController::class);
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
